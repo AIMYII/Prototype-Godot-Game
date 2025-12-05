c2ff4e71a4669ca0e3c2051056b5805afa4697e3
Amiiyah <you@yourdomain.example.com>
2025-12-05 18:11:58 +0000

    Added AI to the mech

--- scripts/mech.gd
+++ scripts/mech.gd
@@ -1,22 +1,44 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 var is_player_in_mech = false
+var player_in_area = false
+var player = null
+
+@export var player_path : NodePath
+
+# State machines
+enum {
+	IDLE,
+	RUNNING,
+	PLAYER_GO_IN,
+	PLAYER_COME_OUT,
+	WAIT
+}
+
+var state = IDLE
+
+func _ready():
+	player = get_node(player_path)
 
 func _physics_process(delta):
+	
+	_mech_AI()
+	
+	match state:
+		RUNNING:
+			_handle_movement(delta)
+	
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
-	_handle_movement(delta)
-		
-	pass
 
 
 func _handle_movement(delta):
 	if is_player_in_mech == true:
+		$dectection_area/CollisionShape2D.disabled = true;
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
@@ -31,12 +53,22 @@ func _handle_movement(delta):
 
 		move_and_slide()
 
+func _on_player_in_mech():
+	is_player_in_mech = true
+
+
+func _on_dectection_area_body_entered(body):
+	if body.is_in_group("Player"):
+		body.state = body.RUNNING
+		player = body
+
+
+func _on_dectection_area_body_exited(body):
+	if body.is_in_group("Player"):
+		body.state = body.IDLE
+		player = body
+
 func _mech_AI():
 	if is_player_in_mech == false:
 		# Run AI code, to follow the player, and commands.
-		pass
-	pass
-
-
-func _on_player_in_mech():
-	is_player_in_mech = true
+		position += (player.position - position) / SPEED
